f330dcf4b8b98ec0ba9d14a3d3d29e4b705f588a
2jin2.1031 <2jin2.1031@gmail.com>
2024-07-23 15:37:45 +0900

    feat: constrctor function

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-function createUser(email, birthdate) {
-        const user = {
-        email,
-        birthdate,
-        buy(item) {
-            console.log(`${this.email} buys ${item.name}`);
-        },
+function User(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+    this.buy = function(item) {
+        console.log(`${this.email} buys ${item.name}`);
     };
-return user;
 }
 
 const item = {
@@ -14,14 +11,12 @@ const item = {
     price: 30000,
 };
 
-const user1 = createUser('test1@naver.com', '1234-05-06');
-const user2 = createUser('test2@naver.com', '1234-07-08');
-const user3 = createUser('test3@naver.com', '1234-09-10');
+const user1 = new User('test1@naver.com', '1234-01-02');
+const user2 = new User('test2@naver.com', '1234-03-04');
+const user3 = new User('test3@naver.com', '1234-05-06');
 
 console.log(user1.email);
 console.log(user2.email);
 console.log(user3.email);
-
+console.log(user1.birthdate);
 user1.buy(item);
-user2.buy(item);
-user3.buy(item);
